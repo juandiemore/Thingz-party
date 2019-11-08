9fb22c5e867906a6971aec62a4b801e5dc8c2ebf
juan <juandiemore@gmail.com>
2019-11-08 12:43:34 -0500

    corriendo

---
+++ The Fran Project/Assets/Scripts/Inicio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inicio : MonoBehaviour
+{   
+    private Animator ini;
+    public GameObject inicio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ini = GetComponent<Animator>();
+        inicio = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.anyKey)
+        {
+            ini.SetBool("logo",true);
+        }
+      
+    }
+    public void LoadMenu(){
+     //   this.SetActive(false);
+        
+    }
+}
---
+++ The Fran Project/Assets/Scripts/MenuTransition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTransition : MonoBehaviour
+{
+
+    private Animator animMenu;
+    private int estado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animMenu = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if(Input.GetKeyDown(KeyCode.W) && estado<=3){
+        estado+=1;
+        
+    }
+        if(Input.GetKeyDown(KeyCode.S) && estado>=0){
+        estado-=1;
+
+    }
+
+    animMenu.SetInteger("state",estado);
+
+    if(Input.GetKeyDown(KeyCode.Return)){
+        animMenu.SetBool("selected",true);
+    }
+    }
+}
