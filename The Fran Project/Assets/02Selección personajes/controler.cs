5caf25c66c774b36c2667b823f854b4ca61549b6
juan <juandiemore@gmail.com>
2019-11-14 13:10:17 -0500

    si se puede

---
+++ The Fran Project/Assets/02Selección personajes/controler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class controler : MonoBehaviour
+{
+
+    private Animator ini;
+    // Start is called before the first frame update
+    void Start()
+    {
+          ini = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+ IEnumerable transicion(){
+     yield return new WaitForSeconds(1.5f);
+     SceneManager.LoadScene("juego");
+ }
+
+}
--- The Fran Project/Assets/Scripts/PlayerAnimator.cs
+++ The Fran Project/Assets/Scripts/PlayerAnimator.cs
@@ -6,15 +6,30 @@ public class PlayerAnimator : MonoBehaviour
 {
 
     private Animator anim;
+
+    float dirX, moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        moveSpeed = 5f;
         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    dirX = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+
+    transform.position = new Vector2 (transform.position.x + dirX, transform.position.y);
+
+    if(dirX != 0){
+        anim.SetBool("IsWalking",true);
+    }else{
+        anim.SetBool("IsWalking",false);
+    }
+
+        
         
     }
 
