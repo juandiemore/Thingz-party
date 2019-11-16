b751de3aa211d079b07acd2587972e0ad0474bca
juan <juandiemore@gmail.com>
2019-11-15 22:21:03 -0500

    adding sounds

--- Thingz-party-Game/Assets/Scripts/MenuTransition.cs
+++ Thingz-party-Game/Assets/Scripts/MenuTransition.cs
@@ -6,31 +6,39 @@ using UnityEngine.SceneManagement;
 public class MenuTransition : MonoBehaviour
 {
 
+    public AudioClip sel;
+    public AudioClip ok;
+    
+    AudioSource audio;
     private Animator animMenu;
     private int estado;
     // Start is called before the first frame update
     void Start()
     {
         animMenu = GetComponent<Animator>();
-
+        audio = GetComponent<AudioSource>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-    if(Input.GetKeyDown(KeyCode.W) && estado<=3){
+    if(Input.GetKeyDown(KeyCode.W) && estado<3){
         estado+=1;
-        
+        audio.clip = sel;
+        audio.Play();
     }
-        if(Input.GetKeyDown(KeyCode.S) && estado>=0){
+        if(Input.GetKeyDown(KeyCode.S) && estado>0){
         estado-=1;
-
+        audio.clip = sel;
+        audio.Play();
     }
 
     animMenu.SetInteger("state",estado);
 
     if(Input.GetKeyDown(KeyCode.Return)){
         animMenu.SetBool("selected",true);
+          audio.clip = ok;
+        audio.Play();
         }
     }
 
@@ -42,7 +50,7 @@ public class MenuTransition : MonoBehaviour
         }else if(estado == 2){
             SceneManager.LoadScene("mapa");
         }else if(estado == 3){
-            SceneManager.LoadScene("pjs");
+            SceneManager.LoadScene("seleccionPlayer1");
         }
     }
 }
---
+++ Thingz-party-Game/Assets/Scripts/PlayerAnimator2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator2 : MonoBehaviour
+{
+
+    private Animator anim;
+
+    float dirX, moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 5f;
+        anim = GetComponent<Animator>();
+        dirX = this.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+  if (Input.GetKeyDown(KeyCode.LeftArrow)){
+      Vector2 position = this.transform.position;
+      position.x  =0.005f* moveSpeed;
+      //transfrorm.position = position;
+
+  }
+
+    dirX = transform.position.x;
+    transform.position = new Vector2 (transform.position.x + dirX, transform.position.y);
+
+    if(dirX != 0){
+        anim.SetBool("IsWalking",true);
+    }else{
+        anim.SetBool("IsWalking",false);
+    }
+
+        
+        
+    }
+
+    
+}
